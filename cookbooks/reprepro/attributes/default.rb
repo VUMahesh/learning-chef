6c1ea122a1ab03099d4cfa13a6891a7259ad29b6
Mahesh <umamaheswar.vummidichetty@ul.com>
2015-08-17 20:13:29 -0700

    new files

---
+++ cookbooks/reprepro/attributes/default.rb
@@ -0,0 +1,24 @@
+default['reprepro']['fqdn'] = node['fqdn']
+default['reprepro']['listen_port'] = 80
+default['reprepro']['repo_dir'] = "/srv/apt"
+default['reprepro']['incoming'] = "/srv/apt_incoming"
+default['reprepro']['description'] = "APT repository at #{node['fqdn']}"
+default['reprepro']['codenames'] = [node['lsb']['codename']]
+default['reprepro']['allow'] = []
+default['reprepro']['pulls']['name'] = node['lsb']['codename']
+default['reprepro']['pulls']['from'] = node['lsb']['codename']
+default['reprepro']['pulls']['component'] = "main"
+default['reprepro']['architectures'] = %w(i386 amd64)
+default['reprepro']['gnupg_home'] = '/root/.gnupg'
+default['reprepro']['enable_repository_on_host'] = false
+default['reprepro']['disable_databag'] = true
+
+default['reprepro']['nginx']['listen_port'] = 80
+default['reprepro']['nginx']['ssl'] = false
+default['reprepro']['nginx']['ssl_certificate'] = nil
+default['reprepro']['nginx']['ssl_certificate_key'] = nil
+default['reprepro']['pgp_email']="mahesh.vummidichetty@ul.com"
+default['gpg']['name']['email']="trusty"
+default['gpg']['user'] ="vagrant"
+default['gpg']['name']['real'] = "vagrant"
+default['reprepro']['server'] = 'apache'
